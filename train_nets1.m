c7195890f9bbe3f0aa24c54affacd1055d2ebb6c
3Disney <victorcauzz@hotmail.com>
2023-09-23 16:53:59 -0300

    Data (EMBR3, PETR3, VALE3 History)

---
+++ train_nets1.m
@@ -0,0 +1,4 @@
+% FASE 1: Coleta de dados:
+% Aqui deverá ser coletado os dados do Excell
+
+% Criação das três redes:
---
+++ train_nets2.m
@@ -0,0 +1,4 @@
+% Coleta de dados:
+% Aqui deverá ser coletado os dados do Excell.
+
+% Criação das três redes:
