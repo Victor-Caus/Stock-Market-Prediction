256457815e556e136b06ec3ace7a8f1f851e9e1c
3Disney <victorcauzz@hotmail.com>
2023-09-27 22:58:42 -0300

    3 complete

--- train_nets3.m
+++ train_nets3.m
@@ -1,55 +1,225 @@
 % 1. Coleta de dados:
 % Aqui deverá ser coletado os dados de entrada e saída do Excell
-P = []; %sera uma matriz onde P(1,:) é da PETR, P(2,:) é da VALE, P(3,:) é da EMBR. 
-T = []; % Tambem uma matriz seguindo a mesma ordem
 
-% OBS: A arquitetura dessa rede foi inventada pelo grupo
-% 2.Construção das Redes de arquitetura MLP, seguindo a ordem convencionada
-nets = cell(1, 3); % vetor contendo as respectivas redes neurais
-for i = 1:3
-    nets(i) = feedforwardnet({15,10}); % 2 camadas internas
-    nets(i) = configure(nets(i),P(i),T(i));
-end
+tablePETR = readtable('PETR3.xlsx');
+tableVALE = readtable('VALE3.xlsx');
+tableEMBR = readtable('EMBR3.xlsx');
+close = cell(1, 3);
+close{1} = tablePETR{:, 8};
+close{2} = tableVALE{:, 8};
+close{3} = tableEMBR{:, 8};
 
-% 3. Pré-processamento dos Dados
-% 3.1) Normalizar os padrões de Treinamento de entrada/saída entre 0 e 1:
-for i = 1:3
-    for j = 1:size(P(i))
-    nets(i).inputs{j}.processParams{2}.ymin = 0;
-    nets(i).inputs{j}.processParams{2}.ymax = 1;
+% Guardar medias e desvio padrao e fazer o z-scaling (Normalização)
+%{
+mean_std = [mean(close{1}), std(close{1});
+                mean(close{2}), std(close{2});
+                mean(close{3}), std(close{3})];
+close{1} = (close{1} - mean_std(1,1)) / mean_std(1,2);
+close{2} = (close{2} - mean_std(2,1)) / mean_std(2,2);
+close{3} = (close{3} - mean_std(3,1)) / mean_std(3,2);
+%}
+
+T = cell(1, 3); % sera uma celula de matrizes onde T{1} é da PETR, T{2} é da VALE, T{3} é da EMBR. 
+nAmostras = floor(size(close{1},1)/10) - 1;
+P = zeros(30, nAmostras); % padrões de entrada
+
+% organizando as amostras
+for i = 1:nAmostras
+    % pegar os fechamentos "atrasados" das três:
+    P(:,i) = [close{1}(10*i - 9 : 10*i) ; close{2}(10*i - 9 : 10*i) ; close{3}(10*i - 9 : 10*i)];
+    
+    % para cada uma, pegar a saída como sendo os fechamentos adiantados:
+    for j = 1:3
+        T{j}(:,i) = [close{j}(10*(i+1) - 9 : 10*(i+1))];
     end
 end
 
-% 3.2) Dividir os dados entre os conjuntos de Treino, Validação e Erro de
-% Teste:
+%Variáveis importantes:
+nSimulacao = 9; % (referente aos ultimos 3 meses 90/10)
+indiceMaxTrein = nAmostras - nSimulacao;
+% Separando os que vao ser treinados do total:
+Ptr = P(:,1:indiceMaxTrein);
+Ttr = cell(1, 3);
 for i = 1:3
-    nets(i).divideFcn = 'dividerand';
-    nets(i).divideParam.trainRatio = 1.00;
-    nets(i).divideParam.valRatio = 0.00;
-    nets(i).divideParam.testRatio = 0.00;
+    Ttr{i} = T{i}(:,1:indiceMaxTrein);
+end
+
+% 2.Construção das Redes de arquitetura MLP, seguindo a ordem convencionada
+nets = cell(1, 3); % vetor contendo as respectivas redes neurais
+for i = 1:3
+    nets{i} = feedforwardnet([15,10],'trainrp'); % 1 camada internas
+    nets{i} = configure(nets{i},Ptr,Ttr{i});
+end
+
+
+% 3. Pré-processamento dos Dados
+% Normalização dos dados:
+for i = 1:3
+    for j = 1:size(P(i))
+        nets{i}.inputs{j}.processParams{2}.ymin = 0;
+        nets{i}.inputs{j}.processParams{2}.ymax = 1;
+    end
+    for j = 1:size(T{1}(i))
+        nets{i}.outputs{j}.processParams{2}.ymin = 0;
+        nets{i}.outputs{j}.processParams{2}.ymax = 1;
+    end
+end
+
+for i = 1:3
+
+    %  Regularização dos dados:
+    nets{i}.performParam.regularization = 0;
+    % 3.1) Dividir os dados, deixando tudo para treino:
+    nets{i}.divideFcn = 'dividerand';
+    nets{i}.divideParam.trainRatio = 1.00;
+    nets{i}.divideParam.valRatio = 0.00;
+    nets{i}.divideParam.testRatio = 0.00;
 end
 
 % 4. Treinamento das redes
 for i = 1:3
-    nets(i).trainParam.showWindow = true;   % Exibe a interface de usuário (GUI)
-    % Arquitetura da rede e funções de ativação de cada camada:
-    nets(i).layers{1}.dimensions = 15;
-    nets(i).layers{1}.transferFcn = 'relu';
-    nets(i).layers{2}.dimensions = 10;
-    nets(i).layers{2}.transferFcn = 'relu';
-    nets(i).layers{3}.transferFc = 'purelin';  % Output como puramente linear
-    nets(i).performFcn = 'mse';         % Usamos somas quadráticas 
-    nets(i).trainFcn = 'trainrp';       % Algoritmo de otimização usado
+    nets{i}.trainParam.showWindow = true;   % Exibe a interface de usuário (GUI)
+      % Arquitetura da rede e funções de ativação de cada camada:
+       % Arquitetura da rede e funções de ativação de cada camada:
+    nets{i}.layers{1}.dimensions = 15;
+    nets{i}.layers{1}.transferFcn = 'poslin';
+    nets{i}.layers{2}.dimensions = 10;
+    nets{i}.layers{2}.transferFcn = 'poslin';
+    nets{i}.layers{end}.transferFcn = 'purelin';  % Output como puramente linear
+    nets{i}.performFcn = 'mse';         % Usamos somas quadráticas 
+    nets{i}.trainFcn = 'trainrp';       % Algoritmo de otimização usado
 
-    % Hiperparametros de treinamentos (Ajustar "na mão"):
-    nets(i).trainParam.epochs = 10000;
-    nets(i).trainParam.time = 120;
-    nets(i).trainParam.lr = 0.2;
-    nets(i).trainParam.min_grad = 10^-8;
-    nets(i).trainParam.max_fail = 1000;
+    % Hiperparâmetros de treinamentos (Ajustar "na mão"):
+    nets{i}.trainParam.epochs = 150;
+    nets{i}.trainParam.time = 120;
+    nets{i}.trainParam.mu = 0.01;
+    nets{i}.trainParam.min_grad = 10^-5; 
+    nets{i}.trainParam.max_fail = 1000;
 
-    [net,tr] = train(net,P(i),T(i));
+    [nets{i},tr] = train(nets{i},Ptr,Ttr{i});
 end
 
-%5. Teste das redes neurais:
-% Ainda a fazer
+% Salvar as redes:
+save('trained_nets_1.mat', 'nets');
+
+% Plotar graficos de treinamento:
+
+X_train = P;
+
+%Grafico treino petrobras:
+X_train = Ptr;
+y_pred = reshape(sim(nets{1}, X_train), [], 1) ;
+%y_pred = y_pred .* train_mean_std(1,2) + train_mean_std(1,1);
+y_train = Ttr{1} ;
+y_train = reshape(y_train, [], 1) ;
+%y_train = y_train .* train_mean_std(1,2) + train_mean_std(1,1);
+figure(1)
+plot(y_train);
+hold on;
+plot(y_pred);
+xlabel('Dia')
+ylabel('Cotação da ação')
+title('Treinamento da rede da PETR3') 
+legend('target','prediction')
+hold off
+
+%Grafico treino VALE:
+X_train = Ptr;
+y_pred = reshape(sim(nets{2}, X_train), [], 1);
+%y_pred = y_pred*train_mean_std(2,2) + train_mean_std(2,1);
+y_train = Ttr{2};
+y_train = reshape(y_train, [], 1);
+%y_train = y_train* train_mean_std(2,2) + train_mean_std(2,1);
+
+figure(2)
+plot(y_train);
+hold on;
+plot(y_pred);
+xlabel('Dia')
+ylabel('Cotação da ação')
+title('Treinamento da rede da VALE3') 
+legend('target','prediction')
+hold off
+
+%Grafico treino EMBRAER:
+X_train = Ptr;
+y_pred = reshape(sim(nets{3}, X_train), [], 1);
+%y_pred = y_pred .* train_mean_std(3,2) + train_mean_std(3,1);
+y_train = Ttr{3};
+y_train = reshape(y_train, [], 1);
+%y_train = y_train * train_mean_std(3,2) + train_mean_std(3,1);
+figure(3)
+plot(y_train);
+hold on;
+plot(y_pred);
+xlabel('Dia')
+ylabel('Cotadição da ação')
+title('Treinamento da rede da EMBR3') 
+legend('target','prediction')
+hold off
+
+%5. Simulacao das redes neurais:
+% Vamos preencher o P e o T simulando aos poucos:
+P_simu = P; 
+T_simu = T;
+
+for j = indiceMaxTrein+1 : nAmostras
+    for i = 1:3
+        T_simu{i}(:,j) = sim(nets{i}, P_simu(:,j));
+    end
+    % "As saídas serão as entradas - Mateus 20:16":
+    P_simu(:,j+1) = [T_simu{1}(:,j) ; T_simu{2}(:,j) ; T_simu{3}(:,j)];
+end
+
+% Plotar gráficos comparativos de cada ação
+xInicio = 1:((nAmostras - nSimulacao)*10); 
+xFinal = ((nAmostras - nSimulacao)*10)+1 : nAmostras*10;
+
+% Ações da Petrobras (1)
+auau = T_simu{1}(:,indiceMaxTrein+1:nAmostras);
+y_pred_test = reshape(auau, [], 1);
+y_test = close{1}((indiceMaxTrein+1)*10 -9:nAmostras*10)';
+
+figure(4)
+plot(y_test);
+hold on;
+plot(y_pred_test);
+xlabel('Dia')
+ylabel('Cotacao da acao')
+title('Fechamento da acao PETR3') 
+legend('target','prediction')
+hold off
+
+
+% Vale do rio doce (2)
+y_pred_test = T_simu{2}(:,indiceMaxTrein+1:nAmostras);
+y_pred_test = reshape(y_pred_test, [], 1);
+y_test = close{2}((indiceMaxTrein+1)*10 -9:nAmostras*10)';
+figure(5)
+plot(y_test);
+hold on;
+plot(y_pred_test);
+xlabel('Dia')
+ylabel('Cotacao da acao')
+title('Fechamento da acao VALE3') 
+legend('target','prediction')
+hold off
+ 
+% Embraer (3)
+y_pred_test = T_simu{3}(:,indiceMaxTrein+1:nAmostras);
+y_pred_test = reshape(y_pred_test, [], 1);
+y_test = close{3}((indiceMaxTrein+1)*10 -9:nAmostras*10)';
+figure(6)
+plot(y_test);
+hold on;
+plot(y_pred_test);
+xlabel('Dia')
+ylabel('Cotacao da acao')
+title('Fechamento da acao EMBR3') 
+legend('target','prediction')
+hold off
+
+
+
+
+
